db6eb0aed1b5b92a2c66ecd9967c185d874b0e22
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2024-03-30 12:32:55 +0530

    Update cleanupbuilds.sh

--- cleanupbuilds.sh
+++ cleanupbuilds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "number of recent files to retain"
 read count
-total_count=`ls -lrt | awk -F " " 'NR>1 {print $NF}' | wc -l
-recent=`expr $total_count - $count`
-head -$total_count output | 
-
+ls -lrt | awk -F " " 'NR>1 {print $NF}' > output
+total=`cat output | wc -l`
+recent=`expr $total - $count`
+head -$recent output | xargs rm -rf
