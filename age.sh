067861c320e19c2dfcb57f20e34e0081211ca967
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2024-03-30 12:42:51 +0530

    age.sh

--- age.sh
+++ age.sh
@@ -5,7 +5,7 @@ read data
 sed -i '1d' "$data"
 while read line
 do
-	age=$(echo "$line" | awk -F " " '{print $3}')
+	age=`echo "$line" | awk -F " " '{print $3}'`
 	if [ "$age" -gt 40 ]
 	then
 		echo "$line" | awk -F " " '{print $2}'
